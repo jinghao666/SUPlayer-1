c8f3741bb0c6c77bc8f6629ac82857e8be57fdfc
sudosurootdev <sudosurootdev@gmail.com>
2015-07-22 23:55:58 -0400

    Initial commit
    Signed-off-by: sudosurootdev <sudosurootdev@gmail.com>

---
+++ app/src/main/java/com/sudosurootdev/suplayer/SCService.java
@@ -0,0 +1,17 @@
+package com.sudosurootdev.suplayer;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+/**
+ * Created by sudosurootdev on 6/18/15.
+ */
+public interface SCService {
+
+    @GET("/tracks?client_id=" + Config.CLIENT_ID)
+    public void getRecentTracks(@Query("created_at[from]") String date, Callback<List<Track>> cb);
+
+}
